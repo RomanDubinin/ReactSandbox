8202dca951639969e1ed0529a8db64ac2a1570e6
RomanDubinin <romandubinin94@gmail.com>
2019-12-26 18:58:31 +0500

    dynamic css classes

--- components/Person/Person.js
+++ components/Person/Person.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Person.css'
 export default (params) => {
+    const inputClasses = ['input']
+
+    if (params.phone !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
     return <div className="Person">
         <h1>{params.name}</h1>
         <h2>{params.surname}</h2>
         <p>{params.phone}</p>
         <p>{params.address}</p>
-        <input type='text' onChange={params.onChangePhone} value={params.phone} />
+        <input
+            type='text'
+            onChange={params.onChangePhone}
+            value={params.phone}
+            className={inputClasses.join(' ')}
+        />
         <button onClick={params.onDelete}>Delete</button>
     </div>
 }
