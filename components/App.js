5d1aad3b019a0a1d8285ac74dc9a1d99e1b61d4b
RomanDubinin <romandubinin94@gmail.com>
2019-12-26 10:25:53 +0500

    refactor

--- components/App.js
+++ components/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Person from './Person'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-          <h1>Hello World</h1>
-          <p>Welcome to my world dude!</p>
-          <Person name='Joe' surname='Jonson' address='His address' phone='8 800 35 35'></Person>
-          <Person name='Frederick' surname='Brooks' address='His address'></Person>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p>Welcome to my world dude!</p>
+                <Person name='Joe' surname='Jonson' address='His address' phone='8 800 35 35'/>
+                <Person name='Frederick' surname='Brooks' address='His address'/>
+            </div>
+        );
+    }
 }
 export default App;
