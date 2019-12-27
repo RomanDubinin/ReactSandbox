24eadc3c43b32e39a9a2c684c6978678dcbf594d
RomanDubinin <romandubinin94@gmail.com>
2019-12-27 11:37:51 +0500

    person to class component

--- components/Person/Person.js
+++ components/Person/Person.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './Person.css'
-export default (params) => {
-    const inputClasses = ['input']
 
-    if (params.phone !== '') {
-        inputClasses.push('green')
-    } else {
-        inputClasses.push('red')
+class Person extends React.Component{
+    render(){
+        const inputClasses = ['input']
+
+        if (this.props.phone !== '') {
+            inputClasses.push('green')
+        } else {
+            inputClasses.push('red')
+        }
+
+        return <div className="Person">
+            <h1>{this.props.name}</h1>
+            <h2>{this.props.surname}</h2>
+            <p>{this.props.phone}</p>
+            <p>{this.props.address}</p>
+            <input
+                type='text'
+                onChange={this.props.onChangePhone}
+                value={this.props.phone}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={this.props.onDelete}>Delete</button>
+        </div>
     }
-
-    return <div className="Person">
-        <h1>{params.name}</h1>
-        <h2>{params.surname}</h2>
-        <p>{params.phone}</p>
-        <p>{params.address}</p>
-        <input
-            type='text'
-            onChange={params.onChangePhone}
-            value={params.phone}
-            className={inputClasses.join(' ')}
-        />
-        <button onClick={params.onDelete}>Delete</button>
-    </div>
 }
+
+export default Person
