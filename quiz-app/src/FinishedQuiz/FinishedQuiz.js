31be21ba1e4a25a6ef53a88d3a456ddfd3684194
RomanDubinin <romandubinin94@gmail.com>
2020-01-01 17:38:00 +0500

    right count

--- quiz-app/src/FinishedQuiz/FinishedQuiz.js
+++ quiz-app/src/FinishedQuiz/FinishedQuiz.js
@@ -2,21 +2,29 @@ import React from "react";
 import './FinishedQuiz.css'
 
 const FinishedQuiz = props =>{
+    const rightAnswers = Object.keys(props.results).reduce((total, key) => {
+        if (props.results[key] === 'correct')
+            total++;
+        return total;
+    }, 0);
+
     return (
         <div className={'FinishedQuiz'}>
-        <ul>
-            {props.quiz.map((questionItem, index) => {
-                const isCorrect = props.results[index] === 'correct';
+            <ul>
+                {props.quiz.map((questionItem, index) => {
+                    const isCorrect = props.results[index] === 'correct';
 
-                return (
-                    <li key={index}>
-                        <strong>{index+1}</strong>.&nbsp;
-                        {questionItem.question}
-                        <i className={isCorrect ? 'correctAnswer' : 'wrongAnswer'}>{isCorrect ? 'v' : 'x'}</i>
-                    </li>
-                )
-            })}
-        </ul>
+                    return (
+                        <li key={index}>
+                            <strong>{index+1}</strong>.&nbsp;
+                            {questionItem.question}
+                            <i className={isCorrect ? 'correctAnswer' : 'wrongAnswer'}>{isCorrect ? 'v' : 'x'}</i>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <p>Right {rightAnswers} from {props.quiz.length}</p>
     </div>)
 };
 
