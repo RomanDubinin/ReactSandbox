e940fc533623ee3f720aa4a1d6587920d884835c
RomanDubinin <romandubinin94@gmail.com>
2020-01-04 15:15:26 +0500

    quiz list

--- quiz-app/src/QuizList/QuizList.js
+++ quiz-app/src/QuizList/QuizList.js
@@ -1,10 +1,37 @@
 import React from "react";
+import './QuizList.css'
+import {NavLink} from "react-router-dom";
 
 export default class QuizList extends React.Component{
+
+    renderQuizList(){
+        const quizes = [
+            {title: 'first', id: '1'},
+            {title: 'second', id: '2'},
+            {title: 'third', id: '3'}
+        ];
+
+        return quizes.map((quiz, index) => {
+            return (
+                <li key={index}>
+                    <NavLink to={'quiz/' + quiz.id}>
+                        Quiz {quiz.title}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz List</h1>
+            <div className={'QuizList'}>
+                <div>
+                    <h1>Quiz List</h1>
+
+                    <ul>
+                        {this.renderQuizList()}
+                    </ul>
+                </div>
             </div>
         );
     }
