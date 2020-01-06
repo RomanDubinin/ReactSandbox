a15fb50e6e81e56f141915c5bb4462f3c73ee1a5
RomanDubinin <romandubinin94@gmail.com>
2020-01-06 17:06:01 +0500

    authorization page without logic

--- quiz-app/src/Auth/Auth.js
+++ quiz-app/src/Auth/Auth.js
@@ -1,10 +1,43 @@
 import React from "react";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+import './Auth.css'
 
 export default class Auth extends React.Component{
+    loginHandler = () => {
+
+    };
+    registerHandler = () => {
+
+    };
+    submitHandler = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={'Auth'}>
+                <div>
+                    <h1>Authorization</h1>
+
+                    <form onSubmit={this.submitHandler} className={'AuthForm'}>
+                        <Input inputType='text' label='Email'/>
+                        <Input inputType='text' label='Password'/>
+
+                        <Button
+                            buttonType='success'
+                            onClick={this.loginHandler}
+                        >
+                            SignIn
+                        </Button>
+                        <Button
+                            buttonType='primary'
+                            onClick={this.registerHandler}
+                        >
+                            SignUp
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
---
+++ quiz-app/src/UI/Input/Input.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Input.css'
+
+const IsInvalid = ({valid, touched, shouldValidate}) => {
+    return !valid && shouldValidate && touched;
+};
+
+const Input = (props) =>{
+    const inputType = props.inputType || 'text';
+    const cls = ['Input'];
+    const htmlFor = `${inputType}-${Math.random()}`;
+    if (IsInvalid(props))
+        cls.push('Invalid')
+    return(
+        <div className={cls.join(' ')}>
+            <label htmlFor={htmlFor}>{props.label}</label>
+            <input
+                type={inputType}
+                id={htmlFor}
+                value={props.value}
+                onChange={props.onChange}
+            />
+
+            {IsInvalid(props) ? <span>{props.errorMessage}</span> : null}
+        </div>
+    )
+};
+
+export default Input
