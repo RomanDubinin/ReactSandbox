c319fb4dee51573d69c8a2969656064688905a52
RomanDubinin <romandubinin94@gmail.com>
2020-01-07 22:43:29 +0500

    QuizCreator page without logic

--- quiz-app/src/QuizCreator/QuizCreator.js
+++ quiz-app/src/QuizCreator/QuizCreator.js
@@ -1,10 +1,56 @@
 import React from "react";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+import './QuizCreator.css'
 
 export default class QuizCreator extends React.Component{
+    state = {
+        quiz: [],
+        formControls: {
+            question: '',
+            option1: '',
+            option2: '',
+            option3: '',
+            option4: '',
+        }
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+    };
+
+    addQuestionHandler = () => {
+
+    };
+
+    createQuizHandler = () => {
+
+    };
+
     render() {
         return (
-            <div>
-                <h1>Quiz Creator</h1>
+            <div className='QuizCreator'>
+                <div>
+                    <h1>Quiz Creator</h1>
+
+                    <form onSubmit={this.submitHandler}>
+                        <Input inputType='text'/>
+                        <hr/>
+                        <Input inputType='text'/>
+                        <Input inputType='text'/>
+                        <Input inputType='text'/>
+                        <Input inputType='text'/>
+
+                        <select></select>
+
+                        <Button buttonType='primary' onClick={this.addQuestionHandler}>
+                            Add question
+                        </Button>
+                        <Button buttonType='success' onClick={this.createQuizHandler}>
+                            Create quiz
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
