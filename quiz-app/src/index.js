a3ac84956fc523e37109443e10fe329d4abc906e
RomanDubinin <romandubinin94@gmail.com>
2020-01-20 19:20:39 +0500

    redux preparations

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import {createStore, compose, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 
+
+// devtools ------------------------------------------------------------
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+// devtools ------------------------------------------------------------
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
+
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
 
 ReactDOM.render(app, document.getElementById('root'));
 
